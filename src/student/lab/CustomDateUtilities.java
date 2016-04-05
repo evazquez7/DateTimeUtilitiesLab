face65bf4829e47165b5c44878765340c5ff2b84
Emilio <Emilio@10.30.10.177>
2016-04-04 19:33:43 -0500

    CustomUtilitiesLabUpdated

--- src/student/lab/CustomDateUtilities.java
+++ src/student/lab/CustomDateUtilities.java
@@ -24,27 +24,38 @@ public class CustomDateUtilities {
     
     /**
      * This method is used to convert a LocalDateTime string to a date object. 
-     * @param dateTimeString
-     * @return a dateTime
+     * @param dateTimeString a string representation of a LocalDateTime
+     * @return a LocalDateTime object
+     * @throws IllegalArgumentException if dateTimeString is null
      */
-        public LocalDateTime toDateTime(String dateTimeString) throw IllegalArgumentException {
-            String dateTimeString = null;
+        public LocalDateTime toDateTime(String dateTimeString) throws IllegalArgumentException {
             if (dateTimeString == null){
-                throw new("LocalDateTime is mandatory");
+                throw new IllegalArgumentException ("Error: dateTimeString cannot be null");
             }
-            
             DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
             LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, format);
             return dateTime;
         }
         
-        public String toString(Calendar date, String format){
-        DateFormat dateFormat = new SimpleDateFormat(format);
-        Date today = Calendar.getInstance().getTime(); 
-        String date1 = dateFormat.format(today);
+        /**
+         * This method is used to convert a date object to a string.
+         * It also accepts the format you want the date to be converted too.
+         * @param date <code>Calender</code> object
+         * @param format a string representation of a date format
+         * @return
+         * @throws IllegalArgumentException if date or format is null
+         */
+        public String toString(Calendar date, String format) throws IllegalArgumentException{
+            if (date == null || format == null ){
+                throw new IllegalArgumentException ("Error: Calender Date or Format cannot be null");
+            }
+            DateFormat dateFormat = new SimpleDateFormat(format);
+            Date today = Calendar.getInstance().getTime(); 
+            String date1 = dateFormat.format(today);
         
             return date1;
         }
+        
         
         public long DateDiffHours(LocalDateTime date1 , LocalDateTime date2) {                
                 Duration diff = Duration.between(date1, date2);
